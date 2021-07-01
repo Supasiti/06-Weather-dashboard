d3be553b084ddd9fce5b7eb8522e5a6f938ce22d
Thara Supasiti <sapiensis@gmail.com>
2021-07-01 16:54:59 +1000

    set up api call with weather

--- assets/js/weather-result.js
+++ assets/js/weather-result.js
@@ -1,6 +1,12 @@
 var formEl = document.querySelector('#city-form');
 var cityInputEl = document.querySelector('#city-name-input');
 
+var weatherAPIbase = 'https://api.openweathermap.org/data/2.5/';
+var coordinatePath = 'weather?q=';
+var forecastPath = 'onecall?';
+var APIkey = '&appid=71b78982268256669dafd58400af0acc';
+
+
 function handleSearchCity(event) {
   event.preventDefault();
   let cityInput = cityInputEl.value.trim();
@@ -14,9 +20,16 @@ function handleSearchCity(event) {
   clearWarning(formEl);
   clearInput(formEl);
 
-  let forecast = getForcast(cityInput);
-  renderTodayForecast(forecast);
-  renderFutureForecast(forecast);
+  getCityCoordinates(cityInput);
+  // console.log(coordinates);
+  // let forecast = getForecast(cityInput);
+  // if (forecast === null) {
+  //   showWarning(formEl, 'This city does not exist. Please check your spelling.');
+  //   return
+  // };
+
+  // renderTodayForecast(forecast);
+  // renderFutureForecast(forecast);
 }
 
 // show a warning to an element
@@ -42,8 +55,61 @@ function clearInput(form) {
   }
 };
 
-function getForcast(cityInput) {
+// return city coordinates
+// catch bad city names here
+function getCityCoordinates(cityInput){
+  let apiUrl = weatherAPIbase + coordinatePath + cityInput + APIkey;
+  let response =  getApi(apiUrl, parseCoordinates, handle404);
+  console.log(response);
+};
+
+// return coordinate data 
+function parseCoordinates(data){
+  console.log(data.coord);
+  return data.coord;
+};
+
+// handle 404 status error
+function handle404(response) {
+  if (response.status == 404) {
+    showWarning(formEl, 'This city does not exist. Please check your spelling.');
+  } else {
+    console.log(response.status);
+  }
   return null;
+};
+
+// get forecast from coord
+function getForecast(coord) {
+  let apiUrl = generateOneCallApiUrl(coord);
+  console.log(apiUrl);
+  // let response = getApi(apiUrl, parseData)
+  return null;
+};
+
+// generate apiUrl for onecall
+function generateOneCallApiUrl(coord) {
+  let baseUrl = weatherAPIbase + forecastPath;
+  let qString = 'lat=' ;
+};
+
+// return response if not ok 
+//  else response data
+function getApi(url, callback, handleError) {
+  fetch(url).then(function (response) {
+    if (response.ok) {
+      return response.json().then(callback);
+    } else {
+      handleError(response); 
+    }
+  }).catch((error) => {
+      console.error('Error:', error);
+  });
+}
+
+function parseData(data) {
+  console.log(data);
+  return data
 };
 
 function renderTodayForecast(forecast){
